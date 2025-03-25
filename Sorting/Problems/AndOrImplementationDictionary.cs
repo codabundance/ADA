1b5e22547144b5fa46fd8494a22be1c090142c0f
Saurabh Singh <saurabh3439.singh@gmail.com>
2025-03-25 23:02:46 +0530

    And Or implementation on dictionary

--- Sorting/Problems/AndOrImplementationDictionary.cs
+++ Sorting/Problems/AndOrImplementationDictionary.cs
@@ -1,8 +1,75 @@
 using System;
 
 namespace Sorting.Problems;
+/*
+Problem: Given a list of words from the book Julius Caesar, and dictionary with each word and then the pages on which that word would appear sorted in ascending order.
+Write code to perform 2 operationd AND and OR such that 
+    AND Operation gives the intersection of page numbers
+    OR operation gives the Union of page numbers
 
+Approach : The best approach is to go through both the arrays using 2 pointers. If elements at both the pointers are equal, we add them to the list. If elements are not equal
+for AND - if ith element is larger, we need to move the j pointer and vice versa, because the array is sorted and We only need to add if elements are equal.
+for OR - if ith element is larger, add the jth element to the result, and increment j and vice versa.
+
+Complexity 
+Time - O(m+n), Space - O(1) 
+*/
 public class AndOrImplementationDictionary
 {
-
+    public void AND(List<int> julius, List<int> caesar)
+    {
+        List<int> result = [];
+        int i=0, j=0;
+        while(i < julius.Count && j < caesar.Count)
+        {
+            if(julius[i]==caesar[j])
+            {
+                result.Add(julius[i]);
+                i++;j++;
+            }
+            else if(julius[i] > caesar[j])
+            {
+                j++;
+            }
+            else if(julius[i]< caesar[j])
+            {
+                i++;
+            }
+        }
+        Console.WriteLine(string.Join(", ", result));
+    }
+    public void OR(List<int> julius, List<int> caesar)
+    {
+        List<int> result = [];
+        int i=0, j=0;
+        while(i < julius.Count && j < caesar.Count)
+        {
+            if(julius[i]==caesar[j])
+            {
+                result.Add(julius[i]);
+                i++;j++;
+            }
+            else if(julius[i] > caesar[j])
+            {
+                result.Add(caesar[j]);
+                j++;
+            }
+            else if(julius[i] < caesar[j])
+            {
+                result.Add(julius[i]);
+                i++;
+            }
+        }
+        while(i < julius.Count)
+        {
+            result.Add(julius[i]);
+            i++;
+        }
+        while(j < caesar.Count)
+        {
+            result.Add(caesar[j]);
+            j++;
+        }
+        Console.WriteLine(string.Join(",",result));
+    }
 }
--- Sorting/ProblemsEntry.cs
+++ Sorting/ProblemsEntry.cs
@@ -18,7 +18,10 @@ public class ProblemsEntry
     */
     public void EnterProblems()
     {
-        var nearest = new NearestPoints();
-        nearest.nearest_neighbours(1,1,1,[[0,0],[1,0]]);
+        //var nearest = new NearestPoints();
+        //nearest.nearest_neighbours(1,1,1,[[0,0],[1,0]]);
+        var JuliusCaesar = new AndOrImplementationDictionary();
+        JuliusCaesar.AND([1,3,4,11,13,15,22,31,34,53], [1,3,5,17,18,21,22,33,34,51]);
+        JuliusCaesar.OR([1,3,4,11,13,15,22,31,34,53], [1,3,5,17,18,21,22,33,34,51]);
     }
 }
